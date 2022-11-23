b17b17291a579e06d71a155f98149d8741e4f20c
peroksiid <78278108+peroksiid@users.noreply.github.com>
2022-11-23 22:38:06 +0200

    Update SignUp.vue

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,53 +1,58 @@
 <template>
-    <div class="app">
-        <p v-if="error.length">
-            <b>Please correct the following errors:</b>
-            <ul>
-                <li v-for="e in error" v-bind:key="e.id">{{e}}</li>
-            </ul>
-        </p>
-        <form @submit="signup">
-            <p>
-                <input type="email" placeholder="Email">
-            </p>
-            <p>
-                <input type="password" placeholder="Password" v-model="pw">
-            </p>
-            <button type="submit">Signup</button>
-        </form>
+    <div id="app" v-cloak>
+        <h2>Password</h2>
+        <input type="email" placeholder="Email">
+        <div class="password">
+            <input :class='{valid:passwordValidation.valid}' v-model="password" placeholder="Password">
+        </div>
+        <button>Login</button>
+        <transition name="hint" appear>
+            <div v-if='passwordValidation.errors.length > 0 && !submitted' class='hints'>
+                <h2>Hints</h2>
+                <p v-for='error in passwordValidation.errors'>{{error}}</p>
+            </div>
+        </transition>	
     </div>
 </template>
 <script>
 export default {
     name:'SignUp',
-    data()
-    {
-        return {
-            error: [],
-            pw:null
-        }
-    },
-    methods:{
-        signup(e)
-        {
-            if(this.pw)
-            {
-                console.log("login function called")
-            }
-            this.error=[];
-            if(!this.pw)
-            {
-                this.error.push("password is required")
-            }
-            console.warn("errors",this.error)
-            e.preventDefault()
-        }
-    }
+    el:"#app",
+	data () {
+		return {
+			rules: [
+				{ message:"8 characters minimum.", regex:/.{8,}/ },
+                { message:"15 characters maximum.", regex:/^.{0,15}$/ },
+				{ message:"One uppercase letter required.",  regex:/[A-Z]+/ },
+                { message:"Two lowercase letters required.", regex:/[a-z].{1,}/ },
+				{ message:"One number required.", regex:/[0-9]+/ },
+                { message:"Password should start with an uppercase letter.", regex:/^[A-Z]/},
+                { message:"Password should include the character '_'.", regex:/[_]/}
+			],
+			password:'',
+			submitted:false
+		}
+	},
+	computed: {
+		passwordValidation () {
+			let errors = []
+			for (let condition of this.rules) {
+				if (!condition.regex.test(this.password)) {
+					errors.push(condition.message)
+				}
+			}
+			if (errors.length === 0) {
+				return { valid:true, errors }
+			} else {
+				return { valid:false, errors }
+			}
+		}
+	}
 }
 </script>
 
 <style scoped>
-.app {
+#app {
     width: 400px;
     margin: 0 auto;
     background-color: #2c3e50;
@@ -68,11 +73,11 @@ input {
     text-align: center;
 }
 button {
-  background-color: #42b983;
-  padding: 10px 20px;
-  margin-top: 10px;
-  border: none;
-  color: white;
-  border-radius: 20px;
+    background-color: #42b983;
+    padding: 10px 20px;
+    margin-top: 10px;
+    border: none;
+    color: white;
+    border-radius: 20px;
 }
 </style>
