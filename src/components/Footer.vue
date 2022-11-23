c3f56e55083499071e2d3ac7aeaccecc6eb51904
Ingrem <tsirkove@gmail.com>
2022-11-23 22:42:54 +0200

    changed footer a bit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,6 +10,7 @@
 <style scoped>
     footer {
 	display: flex;
+    justify-content: center;
 	background-color:	#D3D3D3;
 	border-radius: 10px;
 	margin: 0;
@@ -17,6 +18,7 @@
 	position: fixed;
 	bottom: 0;
 	right: 0;
+    left: 0;
     }
     @media (max-width: 800px) {
         footer {
